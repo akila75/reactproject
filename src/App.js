9ef6bb2bdb6991f7ba725e1a3ddb60641b51f726
Akila <akilaayyanar2007@gmail.com>
2026-06-13 01:46:54 +0530

    React app completely created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <h2>My Name is ______</h2>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
